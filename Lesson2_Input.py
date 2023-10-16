7fad055904a3ebea920b79971f1337c492dcbaa0
VejsilAGA <132472252+VejsilAGA@users.noreply.github.com>
2023-10-16 21:21:55 +0200

    Add files via upload

---
+++ Lesson2_Input.py
@@ -0,0 +1,6 @@
+number1 = input("Enter number 1: ")
+number2 = input("Enter number 2: ")
+number3 = float(input("Enter number 3: "))
+
+sum = float(number1) + float(number2) + number3
+print("The sum of the two numbers is: ", sum)

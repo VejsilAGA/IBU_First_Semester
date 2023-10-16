3293afe80c974c39e74aff9b5d223df4161c3a86
VejsilAGA <132472252+VejsilAGA@users.noreply.github.com>
2023-10-16 21:22:15 +0200

    Add files via upload

---
+++ Lesson2_LogicalOperators.py
@@ -0,0 +1,27 @@
+age = 19
+old_enough = age >= 18
+print(old_enough)
+
+age2= 17
+old_enough = age2 >=18
+print(old_enough)
+
+x = 10
+result = x > 8
+result2 = x<15
+
+y = 10
+result3 = x>8
+result4 = x>10
+
+print(result)
+print(result2)
+
+final_result = result and result2
+print(final_result)
+
+final_result2 = result3 or result4
+print(final_result2)
+
+z = 9
+print(z>0 and z<10)

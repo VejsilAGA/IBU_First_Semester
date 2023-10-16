ff4db21b92fdd8802e0784d58f059340f5771829
VejsilAGA <132472252+VejsilAGA@users.noreply.github.com>
2023-10-16 21:22:42 +0200

    Add files via upload

---
+++ Lesson2_WhileLoop.py
@@ -0,0 +1,16 @@
+i = 0
+while i <=5:
+    i+=1
+    print(i)
+    
+sum = 0
+prod = 1
+i = 4
+
+while i <=11:
+    i += 2
+    sum +=   i
+    prod *= i
+    
+    print("sum: ", sum)
+    print("prod: ", prod)
